6cd7a030c1f5753e88815e54f162134d51c72ace
Peter Hutterer <peter.hutterer@who-t.net>
2014-06-11 09:45:07 +1000

    test: add clickpad software button tests
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>

--- test/touchpad.c
+++ test/touchpad.c
@@ -512,6 +512,377 @@ START_TEST(clickpad_click_n_drag)
 }
 END_TEST
 
+START_TEST(clickpad_softbutton_left)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+
+	litest_drain_events(li);
+
+	litest_touch_down(dev, 0, 10, 90);
+	litest_event(dev, EV_KEY, BTN_LEFT, 1);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+
+	litest_event(dev, EV_KEY, BTN_LEFT, 0);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+	litest_touch_up(dev, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+
+	libinput_dispatch(li);
+
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
+START_TEST(clickpad_softbutton_right)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+
+	litest_drain_events(li);
+
+	litest_touch_down(dev, 0, 90, 90);
+	litest_event(dev, EV_KEY, BTN_LEFT, 1);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+
+	assert_button_event(li,
+			    BTN_RIGHT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+
+	litest_event(dev, EV_KEY, BTN_LEFT, 0);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+	litest_touch_up(dev, 0);
+
+	assert_button_event(li,
+			    BTN_RIGHT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+
+	libinput_dispatch(li);
+
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
+START_TEST(clickpad_softbutton_left_tap_n_drag)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+
+	litest_drain_events(li);
+
+	/* Tap in left button area, then finger down, button click
+		-> expect left button press/release and left button press
+	   Release button, finger up
+		-> expect right button release
+	 */
+	litest_touch_down(dev, 0, 20, 90);
+	litest_touch_up(dev, 0);
+	litest_touch_down(dev, 0, 20, 90);
+	litest_event(dev, EV_KEY, BTN_LEFT, 1);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+	litest_assert_empty_queue(li);
+
+	litest_event(dev, EV_KEY, BTN_LEFT, 0);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+	litest_touch_up(dev, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
+START_TEST(clickpad_softbutton_right_tap_n_drag)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+
+	litest_drain_events(li);
+
+	/* Tap in right button area, then finger down, button click
+		-> expect left button press/release and right button press
+	   Release button, finger up
+		-> expect right button release
+	 */
+	litest_touch_down(dev, 0, 90, 90);
+	litest_touch_up(dev, 0);
+	litest_touch_down(dev, 0, 90, 90);
+	litest_event(dev, EV_KEY, BTN_LEFT, 1);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+	assert_button_event(li,
+			    BTN_RIGHT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+	litest_assert_empty_queue(li);
+
+	litest_event(dev, EV_KEY, BTN_LEFT, 0);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+	litest_touch_up(dev, 0);
+
+	assert_button_event(li,
+			    BTN_RIGHT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
+START_TEST(clickpad_softbutton_left_1st_fg_move)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+	struct libinput_event *event;
+	double x = 0, y = 0;
+	int nevents = 0;
+
+	litest_drain_events(li);
+
+	/* One finger down in the left button area, button press
+		-> expect a button event
+	   Move finger up out of the area, wait for timeout
+	   Move finger around diagonally down left
+		-> expect motion events down left
+	   Release finger
+		-> expect a button event */
+
+	/* finger down, press in left button */
+	litest_touch_down(dev, 0, 20, 90);
+	litest_event(dev, EV_KEY, BTN_LEFT, 1);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+	litest_assert_empty_queue(li);
+
+	/* move out of the area, then wait for softbutton timer */
+	litest_touch_move_to(dev, 0, 20, 90, 90, 20, 10);
+	libinput_dispatch(li);
+	usleep(400000);
+	libinput_dispatch(li);
+	litest_drain_events(li);
+
+	/* move down left, expect motion */
+	litest_touch_move_to(dev, 0, 90, 20, 20, 90, 10);
+
+	libinput_dispatch(li);
+	event = libinput_get_event(li);
+	ck_assert(event != NULL);
+	while (event) {
+		struct libinput_event_pointer *p;
+
+		ck_assert_int_eq(libinput_event_get_type(event),
+				 LIBINPUT_EVENT_POINTER_MOTION);
+		p = libinput_event_get_pointer_event(event);
+
+		/* we moved up/right, now down/left so the pointer accel
+		   code may lag behind with the dx/dy vectors. Hence, add up
+		   the x/y movements and expect that on average we moved
+		   left and down */
+		x += libinput_event_pointer_get_dx(p);
+		y += libinput_event_pointer_get_dy(p);
+		nevents++;
+
+		libinput_event_destroy(event);
+		libinput_dispatch(li);
+		event = libinput_get_event(li);
+	}
+
+	ck_assert(x/nevents < 0);
+	ck_assert(y/nevents > 0);
+
+	litest_event(dev, EV_KEY, BTN_LEFT, 0);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+	litest_touch_up(dev, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
+START_TEST(clickpad_softbutton_left_2nd_fg_move)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+	struct libinput_event *event;
+
+	litest_drain_events(li);
+
+	/* One finger down in the left button area, button press
+		-> expect a button event
+	   Put a second finger down in the area, move it right, release
+		-> expect motion events right
+	   Put a second finger down in the area, move it down, release
+		-> expect motion events down
+	   Release second finger, release first finger
+		-> expect a button event */
+	litest_touch_down(dev, 0, 20, 90);
+	litest_event(dev, EV_KEY, BTN_LEFT, 1);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+	litest_assert_empty_queue(li);
+
+	litest_touch_down(dev, 1, 20, 20);
+	litest_touch_move_to(dev, 1, 20, 20, 80, 20, 10);
+
+	libinput_dispatch(li);
+	event = libinput_get_event(li);
+	ck_assert(event != NULL);
+	while (event) {
+		struct libinput_event_pointer *p;
+		double x, y;
+
+		ck_assert_int_eq(libinput_event_get_type(event),
+				 LIBINPUT_EVENT_POINTER_MOTION);
+		p = libinput_event_get_pointer_event(event);
+
+		x = libinput_event_pointer_get_dx(p);
+		y = libinput_event_pointer_get_dy(p);
+
+		ck_assert(x > 0);
+		ck_assert(y == 0);
+
+		libinput_event_destroy(event);
+		libinput_dispatch(li);
+		event = libinput_get_event(li);
+	}
+	litest_touch_up(dev, 1);
+
+	/* second finger down */
+	litest_touch_down(dev, 1, 20, 20);
+	litest_touch_move_to(dev, 1, 20, 20, 20, 80, 10);
+
+	libinput_dispatch(li);
+	event = libinput_get_event(li);
+	ck_assert(event != NULL);
+	while (event) {
+		struct libinput_event_pointer *p;
+		double x, y;
+
+		ck_assert_int_eq(libinput_event_get_type(event),
+				 LIBINPUT_EVENT_POINTER_MOTION);
+		p = libinput_event_get_pointer_event(event);
+
+		x = libinput_event_pointer_get_dx(p);
+		y = libinput_event_pointer_get_dy(p);
+
+		ck_assert(x == 0);
+		ck_assert(y > 0);
+
+		libinput_event_destroy(event);
+		libinput_dispatch(li);
+		event = libinput_get_event(li);
+	}
+
+	litest_event(dev, EV_KEY, BTN_LEFT, 0);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+	litest_touch_up(dev, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
+START_TEST(clickpad_softbutton_left_to_right)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+
+	litest_drain_events(li);
+
+	/* One finger down in left software button area,
+	   move to right button area immediately, click
+		-> expect right button event
+	*/
+
+	litest_touch_down(dev, 0, 20, 90);
+	litest_touch_move_to(dev, 0, 20, 90, 90, 90, 10);
+	litest_event(dev, EV_KEY, BTN_LEFT, 1);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+
+	assert_button_event(li,
+			    BTN_RIGHT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+	litest_assert_empty_queue(li);
+
+	litest_event(dev, EV_KEY, BTN_LEFT, 0);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+	litest_touch_up(dev, 0);
+
+	assert_button_event(li,
+			    BTN_RIGHT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
+START_TEST(clickpad_softbutton_right_to_left)
+{
+	struct litest_device *dev = litest_current_device();
+	struct libinput *li = dev->libinput;
+
+	litest_drain_events(li);
+
+	/* One finger down in right software button area,
+	   move to left button area immediately, click
+		-> expect left button event
+	*/
+
+	litest_touch_down(dev, 0, 90, 90);
+	litest_touch_move_to(dev, 0, 90, 90, 20, 90, 10);
+	litest_event(dev, EV_KEY, BTN_LEFT, 1);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_PRESSED);
+	litest_assert_empty_queue(li);
+
+	litest_event(dev, EV_KEY, BTN_LEFT, 0);
+	litest_event(dev, EV_SYN, SYN_REPORT, 0);
+	litest_touch_up(dev, 0);
+
+	assert_button_event(li,
+			    BTN_LEFT,
+			    LIBINPUT_BUTTON_STATE_RELEASED);
+
+	litest_assert_empty_queue(li);
+}
+END_TEST
+
 int main(int argc, char **argv) {
 
 	litest_add("touchpad:motion", touchpad_1fg_motion, LITEST_TOUCHPAD, LITEST_ANY);
@@ -534,6 +905,15 @@ int main(int argc, char **argv) {
 	litest_add("touchpad:click", touchpad_btn_left, LITEST_TOUCHPAD, LITEST_CLICKPAD);
 	litest_add("touchpad:click", clickpad_btn_left, LITEST_CLICKPAD, LITEST_ANY);
 	litest_add("touchpad:click", clickpad_click_n_drag, LITEST_CLICKPAD, LITEST_SINGLE_TOUCH);
+
+	litest_add("touchpad:softbutton", clickpad_softbutton_left, LITEST_CLICKPAD, LITEST_APPLE_CLICKPAD);
+	litest_add("touchpad:softbutton", clickpad_softbutton_right, LITEST_CLICKPAD, LITEST_APPLE_CLICKPAD);
+	litest_add("touchpad:softbutton", clickpad_softbutton_left_tap_n_drag, LITEST_CLICKPAD, LITEST_APPLE_CLICKPAD);
+	litest_add("touchpad:softbutton", clickpad_softbutton_right_tap_n_drag, LITEST_CLICKPAD, LITEST_APPLE_CLICKPAD);
+	litest_add("touchpad:softbutton", clickpad_softbutton_left_1st_fg_move, LITEST_CLICKPAD, LITEST_APPLE_CLICKPAD);
+	litest_add("touchpad:softbutton", clickpad_softbutton_left_2nd_fg_move, LITEST_CLICKPAD, LITEST_APPLE_CLICKPAD);
+	litest_add("touchpad:softbutton", clickpad_softbutton_left_to_right, LITEST_CLICKPAD, LITEST_APPLE_CLICKPAD);
+	litest_add("touchpad:softbutton", clickpad_softbutton_right_to_left, LITEST_CLICKPAD, LITEST_APPLE_CLICKPAD);
 
 	return litest_run(argc, argv);
 }
