9de131f08253595821cb94a083dcc1ff62325467
Peter Hutterer <peter.hutterer@who-t.net>
2014-07-16 14:10:08 +1000

    test: fix x/y resolution for a test device
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- test/touch.c
+++ test/touch.c
@@ -72,8 +72,8 @@ START_TEST(touch_abs_transform)
 	bool tested = false;
 
 	struct input_absinfo abs[] = {
-		{ ABS_X, 0, 32767, 75, 0, 0 },
-		{ ABS_Y, 0, 32767, 129, 0, 0 },
+		{ ABS_X, 0, 32767, 75, 0, 10 },
+		{ ABS_Y, 0, 32767, 129, 0, 9 },
 		{ ABS_MT_POSITION_X, 0, 32767, 0, 0, 10 },
 		{ ABS_MT_POSITION_Y, 0, 32767, 0, 0, 9 },
 		{ .value = -1 },
