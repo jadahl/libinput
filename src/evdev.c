jadahl/libinput

commit 9f8edc5fd880e0a9c482b36e6b4120ccc056ee0b

evdev: add helper function to normalize delta coordinates
No functional changes.

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
Reviewed-by: Hans de Goede <hdegoede@redhat.com>

src/evdev.c
@@ -224,6 +224,13 @@ evdev_device_transform_y(struct evdev_device *device,
 	return scale_axis(device->abs.absinfo_y, y, height);
 }
 
+static inline void
+normalize_delta(struct evdev_device *device, double *dx, double *dy)
+{
+	*dx = *dx * (double)device->dpi / DEFAULT_MOUSE_DPI;
+	*dy = *dy * (double)device->dpi / DEFAULT_MOUSE_DPI;
+}
+
 static void
 evdev_flush_pending_event(struct evdev_device *device, uint64_t time)
 {
@@ -243,10 +250,9 @@ evdev_flush_pending_event(struct evdev_device *device, uint64_t time)
 	case EVDEV_NONE:
 		return;
 	case EVDEV_RELATIVE_MOTION:
-		dx_unaccel = device->rel.dx / ((double) device->dpi /
-					       DEFAULT_MOUSE_DPI);
-		dy_unaccel = device->rel.dy / ((double) device->dpi /
-					       DEFAULT_MOUSE_DPI);
+		dx_unaccel = device->rel.dx;
+		dy_unaccel = device->rel.dy;
+		normalize_delta(device, &dx_unaccel, &dy_unaccel);
 		device->rel.dx = 0;
 		device->rel.dy = 0;
 

commit c6d81c84dec9d0ea1ee491563c0116ba5e9ea8a7

evdev: fix inverted mouse normalization
Regression introduced in 9f8edc5fd880e0a9c482b36e6b4120ccc056ee0b where it
changed from delta / (dpi/default) to delta * dpi/default, causing the inverse
effect of what the dpi setting is supposed to achieve.

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
Reviewed-by: Hans de Goede <hdegoede@redhat.com>

src/evdev.c
@@ -230,8 +230,8 @@ normalize_delta(struct evdev_device *device,
 		const struct device_coords *delta,
 		struct normalized_coords *normalized)
 {
-	normalized->x = delta->x * (double)device->dpi / DEFAULT_MOUSE_DPI;
-	normalized->y = delta->y * (double)device->dpi / DEFAULT_MOUSE_DPI;
+	normalized->x = delta->x * DEFAULT_MOUSE_DPI / (double)device->dpi;
+	normalized->y = delta->y * DEFAULT_MOUSE_DPI / (double)device->dpi;
 }
 
 static void