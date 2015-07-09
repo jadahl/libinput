b06a1be01322f17e98b69a3633f0efc7b862e429
Peter Hutterer <peter.hutterer@who-t.net>
2015-07-09 13:47:52 +1000

    udev: add a model-quirks callout
    Some model-specific information isn't available through udev properties. This
    callout is used to query the device directly and set a property that we can
    then match on for the hwdb entries.
    
    This is geared for Elantech and ALPS touchpads where the firmware version is
    the interesting bit. The udev rule is added already to match on that, note
    that the callout doesn't do anything at this point. The various
    touchpad-related things will be added separately.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Reviewed-by: Hans de Goede <hdegoede@redhat.com>

---
+++ udev/libinput-model-quirks.c
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2015 Red Hat, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "config.h"
+
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <libudev.h>
+#include <linux/input.h>
+
+#include "libinput-util.h"
+
+static inline const char *
+prop_value(struct udev_device *device,
+	   const char *prop_name)
+{
+	struct udev_device *parent;
+	const char *prop_value = NULL;
+
+	parent = device;
+	while (parent && !prop_value) {
+		prop_value = udev_device_get_property_value(parent, prop_name);
+		parent = udev_device_get_parent(parent);
+	}
+
+	return prop_value;
+}
+
+static void
+handle_touchpad(struct udev_device *device)
+{
+}
+
+int main(int argc, char **argv)
+{
+	int rc = 1;
+	struct udev *udev = NULL;
+	struct udev_device *device = NULL;
+	const char *syspath;
+
+	if (argc != 2)
+		return 1;
+
+	syspath = argv[1];
+
+	udev = udev_new();
+	if (!udev)
+		goto out;
+
+	device = udev_device_new_from_syspath(udev, syspath);
+	if (!device)
+		goto out;
+
+	if (udev_device_get_property_value(device, "ID_INPUT_TOUCHPAD"))
+		handle_touchpad(device);
+
+	rc = 0;
+
+out:
+	if (device)
+		udev_device_unref(device);
+	if (udev)
+		udev_unref(udev);
+
+	return rc;
+}
